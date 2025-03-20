5ae11c68eb366eb7f8c0e4414aa9c6b7ddcbcaa9
Devrajsahani <sahani_devraj@srmap.edu.in>
2025-03-20 17:21:53 +0000

    conversion topic is completed here

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,4 @@
+// so basically here when we compare all the logical operations we will get the answers in boolean form like true or false 
+// and most of the time we know the answer of the solution 
+console.log(null==0); // so here again js is converted the null as 0 and 0 ==0 it is givign the ouput as false and it can also be converted to NaN
+// === triple check is the strict check and check the crucial condition before taking any decision and also the conversion of the logical oprators can put us in a lot of confusion.
