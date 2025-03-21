77969f9afea08b1d5edeeb415ebec0c4e00ed6b0
Devrajsahani <sahani_devraj@srmap.edu.in>
2025-03-21 18:02:59 +0000

    arrays is completed

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,7 @@
+const marvel_heroes=["thor", "Ironman", "spiderman"];
+const dc= ["batman", "superman"];
+// marvel_heroes.push(dc)
+// console.log(marvel_heroes)
+const all_heroes= marvel_heroes.concat(dc)
+console.log(all_heroes)
+// array.from function convert all the values given into the array form.
