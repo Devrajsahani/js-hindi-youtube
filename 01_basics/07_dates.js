f2ab95ff7bc3f5bb0dd3a7fbe089cba03c978d6e
Devrajsahani <sahani_devraj@srmap.edu.in>
2025-03-21 16:45:09 +0000

    dates and all the other topics related to this was covered here.

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,8 @@
+// this date function isnot yet out or not being into the run completely so the files are not accessible as math library.
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+// Fri Mar 21 2025 14:34:06 GMT+0000 (Coordinated Universal Time)
+// Fri Mar 21 2025
+// 3/21/2025, 2:34:06 PM these are the vlaues which we got 
