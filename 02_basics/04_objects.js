1fe118dd26bdd29930eb00f4f3bc8e24038cfbae
Devrajsahani <sahani_devraj@srmap.edu.in>
2025-03-21 20:25:16 +0000

    object along with api and json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -5,20 +5,33 @@
 // tinderUser.isLoggedIn= false;
 // console.log(tinderUser)
 
-const regularUser= { // object ke andar object
-    email:"some@gmail.com",
-    fullname:{
-        Userfullname:{
-            firstname: "devraj",
-            lastname: "sahani"
+// const regularUser= { // object ke andar object
+//     email:"some@gmail.com",
+//     fullname:{
+//         Userfullname:{
+//             firstname: "devraj",
+//             lastname: "sahani"
 
-        }
-    }
-}
-console.log(regularUser.fullname) // so if the vlaue is not there then we cnause ? to show that if the value is there or not, helpful in API calling 
-const obj1= {1:"a", 2:"b"}
-const obj2={3:"C", 4:"D"}
-const obj3= Object.assign(obj1, obj2)// the answer which will come here will be like a string in a array, 
-console.log(obj3);
+//         }
+//     }
+// }
+// console.log(regularUser.fullname) // so if the vlaue is not there then we cnause ? to show that if the value is there or not, helpful in API calling 
+// const obj1= {1:"a", 2:"b"}
+// const obj2={3:"C", 4:"D"}
+// const obj3= Object.assign(obj1, obj2)// the answer which will come here will be like a string in a array, 
+// console.log(obj3);
 // we cna also take the keys and convert it inot the array and then the array operations like loops and other things cna be operated in it.\
 // hasOwnProperty('name') it shows if that is there in the data or not.
+
+// destructring of the objects
+const course= {
+    coursename:"js in hindi",
+    price: "99",
+    courseInstructor:"devraj"
+}
+const {courseInstructor}= course
+console.log(courseInstructor)
+
+// all the api callings are done on the objects and the json are the objects only directly or in directly 
+// some times the apis are also fetched in the form of array 
+// and there are a lot of tools in the market whcih can explain the api structure like json text extractor,
