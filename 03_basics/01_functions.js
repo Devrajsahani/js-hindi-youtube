534087b9ea3731182cd0628a0bbe6bfcf4d2f0eb
Devrajsahani <sahani_devraj@srmap.edu.in>
2025-03-22 20:21:10 +0000

    function basics is covered.

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,2 +1,51 @@
 // function ke andar jo value daal rhe hai vo hote hai parameters 
 // and when funtion calling pe jab daalte hai then it si called arguements
+// function saymyName(){
+//     console.log("H");
+//     console.log("I");
+//     console.log("S");
+//     console.log("E");
+//     console.log("N");
+//     console.log("B");
+//     console.log("E");
+//     console.log("R");
+//     console.log("G");
+// }
+
+// saymyName()
+
+// function addtwoNumbers (number1, number2){
+//     console.log(number1+number2); // once i return something in js then nothing will print after it you have to break the code in order to return something.
+// }
+
+// const result = addtwoNumbers(3,4);
+
+// console.log("Result:", result);
+
+
+// function LoginUserMessage(username){
+//     if(username== undefined){
+//         console.log("please enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+
+
+// funtions with objects and arrays. (... triple dot is also an operator called rest operator or spread operator, but it depends on the usecase of these function, like when are they used)
+function calculateCartPrice(){
+    return num1
+}
+// console.log(calculateCartPrice(200,300,400)) 
+
+const user={
+    username:"hitest",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+handleObject(user)
+// so here the real problem arrises when  
