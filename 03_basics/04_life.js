dcaeb11f84b9cf887e5f8f938ade90cdfad4b142
Devrajsahani <sahani_devraj@srmap.edu.in>
2025-03-23 13:26:31 +0000

    IIFE explained

---
+++ 03_basics/04_life.js
@@ -0,0 +1,6 @@
+(function chai(){
+    console.log("DB connected")
+})()
+// now here to envoke the LIFE immedietly invoked fucntion expression we put the function in the () and then to call the duntion we simply put the () outside of the fucntion or object 
+// global scope ke pollution se bhaut dikkat hoti hai kai baar that is why to decrease the chances of error we can use the iife fucntion 
+// named IIFE is written like the function whereas the unnamed IIFE is written like the arrow function.
