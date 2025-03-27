de3c9e5b54a7aee4c335df6c30212718d422fd86
Devrajsahani <sahani_devraj@srmap.edu.in>
2025-03-27 05:50:41 +0000

    maps and some part are done half only

---
+++ 05_iterators/three.js
@@ -0,0 +1,14 @@
+const arr = [1,2,3,4,5]
+// for of loop used for arrays of high order. 
+for (const num of arr) {
+    // console.log(num); // here num is the iterators like we use the i, j or anything ot signify the iterators that's it,
+    
+}
+const greetings= 'Hello break world' ;
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`) // like if we want to use the break function like if the space is reached then the loop should break then using if we can do it.
+    
+}
+// maps are known for unique entries and also all the values are in the way that they are written and 
+const map= new Map()
+map.
