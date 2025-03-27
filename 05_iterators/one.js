c2dec411e53fd6fc2cb82199e3f50fcf8ed3021c
Devrajsahani <sahani_devraj@srmap.edu.in>
2025-03-27 05:48:31 +0000

    al the basics loop is done

---
+++ 05_iterators/one.js
@@ -0,0 +1,18 @@
+// for 
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+
+// to change all the same variable or values at the same time click on hte element and press ctrl+d to take all the cursor to the element you have selected then change it.
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Outer loop value:${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(i + '*' + j + ' = ' + i*j);
+    }
+    
+}
+
+// table from 0 to 10 how to use loops in js 
+
+// break and continue 
+// break i know so for continue it will print the considition which is atatched to the continue then it will move out of it, like it willmake the mistake happen theignore for the first time.
