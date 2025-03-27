7812df6ec0630dd6ff8dfbf6182b6886862ce47a
Devrajsahani <sahani_devraj@srmap.edu.in>
2025-03-27 05:48:48 +0000

    simple and small part of while loop

---
+++ 05_iterators/two.js
@@ -0,0 +1,5 @@
+let score = 11;
+do {
+    console.log(`score is ${score}`)
+} while (score<=10);
+// so here even though the score value is more than the condition given it will print it and then stop because in do while first the element is printed then the condition is checked.
