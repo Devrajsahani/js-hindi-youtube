29d0cbb3c63f315b033b836955fc0cf6c6e58aa1
Devrajsahani <sahani_devraj@srmap.edu.in>
2025-03-27 05:48:08 +0000

    work flow of code completed

--- 03_basics/workflowofcode.js
+++ 03_basics/workflowofcode.js
@@ -3,5 +3,12 @@
 // the js code is runned in 2 phase 1st one is the memory creation phase then execution phase 
 // so first the code is runned inthe memory creation phase then all the values are stored in the undefined data type then a memory is created that is the variable environment+ execution thread then the code is executed and that environment is deleted after the completion 
 // and all this things are happening in the stack so it follows LIFO concept
+// it is called the nullish coalescing operator (??): null defined
+//the null and undefined ? function is only made for these 2 only 
 
-// 
+// if we want to check the length of the object then we only do it by taking the keys of the objects like (object.keys(emptyobj).lenght ===0){
+//console.log("object in empty");}
+
+// terniary operator
+const icePower= 100;
+icePower<= 80? console.log("less than 80"): console.log("more than 80")
