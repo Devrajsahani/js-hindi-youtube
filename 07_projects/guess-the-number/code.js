c30d0e2ca6999ef306f5169e0363b53f0cc03601
Devraj Sahani <sahani_devraj@srmap.edu.in>
2025-05-29 07:08:55 +0000

    half code of project

---
+++ 07_projects/guess-the-number/code.js
@@ -0,0 +1,31 @@
+const Randomnumber=parseInt(Math.random() *100 + 1);
+
+const submit = document.querySelector('#submit');
+const userinput = document.querySelector('#guessfield');
+const guesslot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastslot');
+const loworhi = document.querySelector('.loworhi');
+const startover = document.querySelector('.resultparas');
+
+const p = document.createElement('p');
+
+let prevguess =[]
+let numguess = 1;
+
+let playgame= true;
+
+function validateguess(guess){
+    //
+  
+}
+
+function checkguess(guess){
+    //
+
+}
+
+function displaymessage(guess){
+    //
+
+}
+
