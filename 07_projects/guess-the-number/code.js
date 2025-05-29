a64095c6294b75bc3d6e7b42433b5789e29e5d7b
Devraj Sahani <sahani_devraj@srmap.edu.in>
2025-05-29 13:46:26 +0000

    the code is still not done and project is incomplete

--- 07_projects/guess-the-number/code.js
+++ 07_projects/guess-the-number/code.js
@@ -14,13 +14,49 @@ let numguess = 1;
 
 let playgame= true;
 
+if(playgame==true){
+    submit.addEventListener('click',function(e){
+    e.preventDefault();
+    const guess= parseInt(Event);
+    console.log(guess);
+    validateguess(guess);
+
+});
+
 function validateguess(guess){
-    //
+    if(NaN (guess)){
+        alert('Please add a valid number')
+    }
+    else if(guess<1){
+        alert('Please enter a valid number')
+    }
+    else if (guess>100){
+        alert('Please enter a valid number')
+    }
+    else{
+        prevguess.push(guess)
+        if(numguess==11){
+            displayguess(`game over, random number was ${Randomnumber}`)
+            endGame()
+
+        }
+    }
   
 }
 
 function checkguess(guess){
-    //
+    if(Randomnumber==guess){
+        displaymessage(`You have guessed the number right, and your score is ${guess}`)
+    }
+    else if(guess<Randomnumber){
+        displaymessage('the number is lesser than what you have entered')
+    }
+    else if(guess>Randomnumber{
+        displaymessage('the number is higher than what you have entered')
+    }
+    else{
+        endGame();
+    }
 
 }
 
@@ -28,4 +64,19 @@ function displaymessage(guess){
     //
 
 }
+function displayguess(guess){
+    userinput.value='';
+    guesslot.innerHTML+=`${guess};   `;
+    numguess++;
+    remaining.innerHTML=`${11- numguess},  `;
+}
+function endgame(){
+    userinput.value =''
+    userinput.setAttribute('disabled', '');
+    p.classList.add('button');
+    p.innerHTML=`<h2 id="newGame">Start new game </h2>`;
+    startover.appendChild(p);
+    
 
+
+}
