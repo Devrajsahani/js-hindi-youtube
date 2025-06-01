a19589a8a3e65b8ce2573cc6d1128ae13d9fbefb
Devraj Sahani <sahani_devraj@srmap.edu.in>
2025-06-01 15:43:19 +0000

    all the elementof the promises are explained

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -11,3 +11,45 @@ const promiseOne = new Promise(function(resolve, reject) {
 promiseOne.then(function(){
     console.log("promise is consumed");
 })
+// there is also a different way to write this promise code which is using the .then fucntion just after the promise is created.
+const promiseTwo = new Promise(function(resolve, reject) {
+    //do a async task 
+    // db calls , crytography, network calls 
+    setTimeout(function(){
+        console.log("Asyn task is completed");
+        resolve()
+    },1000)
+}).then(function(){
+    console.log("promise is created");
+})
+
+const promisethree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username:"devraj", email: "sucksahani@gmail.com"}) // here resolve ke andar koi bhi parametere pass kar sakte hai and it will show it in the terminal.
+
+    },1000)
+}).then(function(user){
+    console.log("user is created", user);
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username:"devraj", password: "124"})
+        }else{
+            reject("Error: something went wrong")
+        }
+    },1000)
+promiseFour.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((username)=>{
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(function(){
+    console.log("the promise is either resolbed or rejected");
+})
+// here we are using the concept of the nested then functions in order to carry out all the things in a single promise.
+// finally is used to execute the code after the promise is either resolved or rejected.
